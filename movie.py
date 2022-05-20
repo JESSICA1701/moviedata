aa170b48eb8a9b4beeea20de63550bb52e9a4386
JESSICA1701 <74467344+JESSICA1701@users.noreply.github.com>
2022-05-20 23:38:28 +0530

    Update movie.py

--- movie.py
+++ movie.py
@@ -7,7 +7,7 @@ print(con)
 cur = con.cursor()
 
 #to create table name Movies
-cur.execute('CREATE TABLE IF NOT EXISTS Movies(Movie VARCHAR,LeadActor VARCHAR,ReleaseYear INT,Director VARCHAR)')
+cur.execute('CREATE TABLE IF NOT EXISTS Movies(MovieName VARCHAR,ActorName VARCHAR,YearOfRelease INT,Director VARCHAR)')
 print('Movie table has been created in Movie database')
 
 #to commit changes
