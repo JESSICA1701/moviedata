88d535c47d9e84c47fc73cbfef8bde46f9292655
JESSICA1701 <74467344+JESSICA1701@users.noreply.github.com>
2022-05-20 23:52:49 +0530

    Create moviess.py

---
+++ moviess.py
@@ -0,0 +1,16 @@
+#to import sqlite3
+import sqlite3
+
+con = sqlite3.connect('Movies.db')
+cur = con.cursor()
+
+#run query to show all the movie details
+cursor = cur.execute("SELECT * FROM Movies")
+print(cur.fetchall())
+
+# Query to fetch only movie name
+cur.execute("SELECT MovieName From Movies ")
+print(cur.fetchall())
+
+con.commit()
+con.close()
