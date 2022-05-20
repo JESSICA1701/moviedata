aa506171ac80fd8fbd006771b97fc590e778e589
JESSICA1701 <74467344+JESSICA1701@users.noreply.github.com>
2022-05-20 23:56:55 +0530

    Update moviedata.py

--- moviedata.py
+++ moviedata.py
@@ -7,11 +7,11 @@ print(con)
 con.cursor()
 
 #Inserting values into table
-con.execute("INSERT INTO Movie VALUES('Life of pi','Mark ','2010','FOXCENTURY')")
-con.execute("INSERT INTO Movie VALUES('zero to hero ','John ','2013','Kiran')")
-con.execute("INSERT INTO Movie VALUES('15 days','Sam ','2031','Disney')")
-con.execute("INSERT INTO Movie VALUES('Haloween','David','2043','Youtube')")
-print("The data has been inserted into the table Movie")
+con.execute("INSERT INTO Movies VALUES('Life of pi','Mark ','2010','FOXCENTURY')")
+con.execute("INSERT INTO Movies VALUES('zero to hero ','John ','2013','Kiran')")
+con.execute("INSERT INTO Movies VALUES('15 days','Sam ','2031','Disney')")
+con.execute("INSERT INTO Movies VALUES('Haloween','David','2043','Youtube')")
+print("The data has been inserted into the table Movies")
 
 #commit changes
 con.commit()
